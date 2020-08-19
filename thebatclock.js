fc2f43654e170b578a41e62dc81b8cf53394f096
Mohsen Bakhit <bakhitmohsen1047@gmail.com>
2020-08-18 21:55:12 -0700

    Debug JS

--- thebatclock.js
+++ thebatclock.js
@@ -1,7 +1,7 @@
 let currentTime = new Date();
 let currentHours = currentTime.getHours();
 let currentMinutes = currentTime.getMinutes();
-if (currentMinute < 10){
+if (currentMinutes < 10){
     currentMinutes = `0${currentTime.getMinutes()}`;
 }
 let currentSeconds = currentTime.getSeconds();
@@ -11,16 +11,16 @@ if (currentSeconds < 10){
 let clockTime = `${currentHours} : ${currentMinutes} : ${currentSeconds}`;
 console.log(clockTime);
 document.getElementById("time").innerHTML = clockTime;
-if (currentHour >= 21) {
+if (currentHours >= 21) {
     document.getElementById("gif").src="https://giphy.com/embed/9ySjUSpUbFMre";
     document.getElementById("message").innerHTML = "It's time to go";
-}else if (currentHour > 19){
+}else if (currentHours >= 19){
     document.getElementById("gif").src="https://giphy.com/embed/cP56PsG4vgCWOSQTsG";
     document.getElementById("message").innerHTML = "Going DARK";
-} else if (currentHours > 15 && currentHours < 18){
+} else if (currentHours >= 15){
     document.getElementById("gif").src="https://giphy.com/embed/VSGo8jRxzsmQw";
     document.getElementById("message").innerHTML = "CHEERS!";
-} else if (currentHours > 11 && currentHours < 13){
+} else if (currentHours >= 13){
     document.getElementById("gif").src="https://giphy.com/embed/ZFJYcVE5lYvWE";
     document.getElementById("message").innerHTML = "Back to work";
-}
+}  
